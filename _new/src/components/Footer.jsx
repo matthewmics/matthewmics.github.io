60162e5bb988e8d567b759bb293c4c498a2aebca
matthew-pixelhouse <matthew@pixelhouse.com.au>
2025-08-31 22:39:20 +0800

    Update Footer

--- _new/src/components/Footer.jsx
+++ _new/src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='py-12 px-4 bg-card relative border-t border-border mt-12 pt-8'>
         <p className='text-sm '>
-            &copy; {new Date().getFullYear()} Matthew Mics. All rights reserved.
+            &copy; {new Date().getFullYear()} Matthew Miclat. All rights reserved.
         </p>
     </footer>
   )
